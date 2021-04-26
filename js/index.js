410a649e45570a6b119b5db905aa622f8a51c526
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2021-04-26 15:39:56 +0200

    added api

--- js/index.js
+++ js/index.js
@@ -1,12 +1,21 @@
-const url = "https://larsingeprojects.one/gamehub/wp-json/wc/store/products";
+const url = "https://larsingeprojects.one/gamehub/wp-json/wc/v3/products";
+
+const fullUrl = "https://noroffcors.herokuapp.com/" + url;
+
+const options = {"headers": {
+    "key": "ck_50303ce64a1724bfa857dcf781f9a6a1823fd78e",
+    "secret": "cs_a155db997c4ff7921cbbdd3911cf931afc5a4068",
+}};
 
 async function blogList() {
     try {
-        const response = await fetch(url);
-        const results = await response.json(response);
+        const response = await fetch(fullUrl, options);
+        const results = await response.json();
         console.log(results);
     }
     catch(error) {
         console.log(error);
     }
 }
+
+blogList();
