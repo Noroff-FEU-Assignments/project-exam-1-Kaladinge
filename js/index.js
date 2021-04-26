a67e394ab43add5cc5d82c808139ccb71ea83984
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2021-04-26 15:17:06 +0200

    added url

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const url = "https://larsingeprojects.one/gamehub/wp-json/wc/store/products";
 async function blogList() {
     try {
         const response = await fetch(url);
-        const results = await response.json();
+        const results = await response.json(response);
         console.log(results);
     }
     catch(error) {
