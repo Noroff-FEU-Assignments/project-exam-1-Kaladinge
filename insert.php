e82bb599091af54f9e124b701d36c7baaea4826f
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2021-05-18 14:04:30 +0200

    tried another php method for saving data

--- insert.php
+++ insert.php
@@ -1,72 +1,27 @@
 <?php
-$fullName = $_POST['name'];
-$Email = $_POST['email'];
-$titleSubject = $_POST['subject'];
-$fullMessage = $_POST['message'];
-
-/*if (!empty($name) || !empty($email) || !empty($subject) || !empty($message)) {*/
-    $host = "10.27.18.48";
-    $dbUsername = "larsingeprojects_oneprojects";
-    $dbPassword = "1Buckethead";
-    /*$dbname = "larsingeprojects_oneprojects";*/
-
-    // create connection
-$conn = mysqli_connect($host, $dbUsername, $dbPassword/*, $dbname*/);
-
-    if(!$conn)
-    {
-        echo 'Not connected';
-    }
-
-    if(!mysqli_select_db($conn,'larsingeprojects_oneprojects'))
-    {
-        echo 'Database not selected'
-    }
-
-    $sql = "INSERT INTO contact (fullName,Email,titleSubject,fullMessage) VALUES ('$fullName','$Email','$titleSubject','$fullMessage')";
-
-    if(!mysqli_query($conn,$sql))
-    {
-        echo 'Not inserted';
-    }
-    else 
-    {
-        echo 'Inserted';
-    }
-
-    header("refresh:2; url=contact.html");
-
-    /*
-    if (mysqli_connect_error()) {
-        die('Connect Error('. mysqli_connect_error().')'. mysqli_connect_error());
-    } else {
-        $SELECT = "SELECT email From contact Where email = ? Limit 1";
-        $INSERT = "INSERT Into contact (name, email, subject, message) values(?, ?, ?, ?)";
-
-        //Prepare statement
-        $stmt = $conn->prepare($SELECT);
-        $stmt->bind_param("s", $email);
-        $stmt->execute();
-        $stmt->bind_result($email);
-        $stmt->store_result();
-        $rnum = $stmt->num_rows;
-
-        if ($rnum==0) {
-            $stmt->close();
-
-            $stmt = $conn->prepare($INSERT);
-            $stmt->bind_param("ssss", $name, $email, $subject, $message); 
-            $stmt->execute();
-            echo "New record inserted successfully";
-        } else {
-            echo "Someone already registered using this email";
-        }
-        $stmt->close();
-        $conn->close();
-    }
-} else {
-    echo "All fields are required";
-    die();
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("larsingeprojects.one.mysql", "larsingeprojects_oneprojects", "1Buckethead", " larsingeprojects_oneprojects");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-?>*/
-    ?>
+ 
+// Escape user inputs for security
+$full_name = mysqli_real_escape_string($link, $_REQUEST['full_name']);
+$email = mysqli_real_escape_string($link, $_REQUEST['email']);
+$full_subject = mysqli_real_escape_string($link, $_REQUEST['full_subject']);
+$full_message = mysqli_real_escape_string($link, $_REQUEST['full_message']);
+ 
+// Attempt insert query execution
+$sql = "INSERT INTO persons (full_name, email, full_subject, full_message) VALUES ('$full_name', '$email', '$full_subject', '$full_message')";
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+?>
